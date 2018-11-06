8677861fc256e7afd7db050030bbe328532e03a9
Benyamin <benjamingenius05@gmail.com>
2018-11-06 04:56:01 +0330

    Updates

---
+++ precache-manifest.559f514b1e37ac5e880b85456ba2960b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d94e68ed29811f18212",
+    "url": "/myresume/static/css/main.8ebf6f78.chunk.css"
+  },
+  {
+    "revision": "2d94e68ed29811f18212",
+    "url": "/myresume/static/js/main.2d94e68e.chunk.js"
+  },
+  {
+    "revision": "7fe1971c1daed02bbb6a",
+    "url": "/myresume/static/js/1.7fe1971c.chunk.js"
+  },
+  {
+    "revision": "82b70cd3742f21482d55",
+    "url": "/myresume/static/js/runtime~main.82b70cd3.js"
+  },
+  {
+    "revision": "99c9f93a7c53a60515effebd7421bd9c",
+    "url": "/myresume/index.html"
+  }
+];
